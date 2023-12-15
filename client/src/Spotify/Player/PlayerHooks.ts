c4f1d70e0fd99bea7f34fe6d78c15cbd90720f61
George Beckert <georgebeckert@gmail.com>
2023-12-14 19:47:26 -0500

    basic playback functionality

--- client/src/Spotify/Player/PlayerHooks.ts
+++ client/src/Spotify/Player/PlayerHooks.ts
@@ -1,10 +1,65 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useSpotify } from "..";
+import { Track } from "@spotify/web-api-ts-sdk";
 
 export function useGetSpotifyPlaybackState() {
   const sdk = useSpotify();
   const queryData = useQuery(["playbackState"], () => {
-    return sdk.player.getPlaybackState()
+    return sdk.player.getPlaybackState();
   });
   return queryData;
+}
+export function usePlayPause() {
+  const sdk = useSpotify();
+  const queryClient = useQueryClient();
+  const { data: playbackState } = useGetSpotifyPlaybackState();
+  const deviceId = playbackState?.device.id;
+  const queryData = useMutation(
+    ["playpause"],
+    async () => {
+      if (deviceId && !queryData?.isLoading) {
+        return playbackState?.is_playing
+          ? sdk.player.pausePlayback(deviceId)
+          : sdk.player.startResumePlayback(deviceId);
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("playbackState");
+      },
+    }
+  );
+  return queryData;
+}
+export function useGetContextPlaylist() {
+  const sdk = useSpotify();
+  const queryClient = useQueryClient();
+  const { data: playbackState } = useGetSpotifyPlaybackState();
+  const playbackContext = playbackState?.context;
+  const playlistId = playbackContext?.href.split("/").at(-1);
+  const queryData = useQuery(
+    ["currentPlaybackContext", playlistId],
+    () => {
+      if (playbackContext?.type !== "playlist") {
+        throw new Error("cannot get context unless inside playlist");
+      }
+      if (!playlistId) {
+        throw new Error("no playlist in context");
+      }
+      return sdk.playlists.getPlaylist(playlistId);
+    },
+    { enabled: !!playlistId }
+  );
+  return queryData;
+}
+export function useGetNextSong(currentSong?: Track) {
+  const query = useGetContextPlaylist();
+  const tracks = query.data?.tracks.items;
+  if (tracks && currentSong) {
+    const currentIndex = tracks?.findIndex(
+      (track) => track.track.id === currentSong.id
+    );
+    return { ...query, data: tracks[(currentIndex + 1) % tracks?.length] };
+  }
+  return { ...query, data: undefined };
 }
--- client/src/SpotifyTree/CurrentSongNode/CurrentSongNode.tsx
+++ client/src/SpotifyTree/CurrentSongNode/CurrentSongNode.tsx
@@ -1,30 +1,41 @@
 import React, { PropsWithChildren } from "react";
 import * as TrackVisualizer from "../TrackVisualizer";
-import { useGetSpotifyPlaybackState } from "../../Spotify/Player/PlayerHooks";
+import {
+  useGetContextPlaylist,
+  useGetNextSong,
+  useGetSpotifyPlaybackState,
+  usePlayPause,
+} from "../../Spotify/Player/PlayerHooks";
+import { Episode, Track } from "@spotify/web-api-ts-sdk";
+
+function asTrack(item: Track | undefined | Episode) {
+  return item && "artists" in item ? (item as Track) : undefined;
+}
 
 export const CurrentSongNode: React.FC<PropsWithChildren> = () => {
-  const { data: playbackState,isLoading:isPlaybackStateLoading } = useGetSpotifyPlaybackState();
-  const track =
-    playbackState?.item && "artists" in playbackState?.item
-      ? playbackState?.item
-      : undefined;
+  const { data: playbackState, isLoading: isPlaybackStateLoading } =
+    useGetSpotifyPlaybackState();
+  const currentTrack = asTrack(playbackState?.item);
+  const { mutate } = usePlayPause();
+  const { data: nextTrackData } = useGetNextSong(currentTrack);
+  const nextTrack = asTrack(nextTrackData?.track);
 
   return (
     <>
-      <TrackVisualizer.TrackTitle track={track} />
-      <TrackVisualizer.TrackArtists track={track} />
+      <TrackVisualizer.TrackTitle track={currentTrack} />
+      <TrackVisualizer.TrackArtists track={currentTrack} />
       <div
         className="cursor-pointer mt-4"
         onClick={() => {
-          /* TODO play/pause */
+          mutate();
         }}
       >
         <TrackVisualizer.TrackVisualizer
-          track={track}
-          duration={track?.duration_ms}
+          track={currentTrack}
+          duration={currentTrack?.duration_ms}
           position={playbackState?.progress_ms}
-          isPaused={playbackState?.is_playing}
-          nextTrack={undefined}
+          isPaused={!playbackState?.is_playing}
+          nextTrack={nextTrack}
           isLoading={isPlaybackStateLoading}
         />
       </div>
