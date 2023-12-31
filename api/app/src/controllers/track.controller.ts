9f0b83770ee057c7089d134a09ebf39b0c5b658b
George Beckert <georgebeckert@gmail.com>
2023-12-31 14:45:21 -0500

    [api]initial m:n relationshop

--- api/app/src/controllers/track.controller.ts
+++ api/app/src/controllers/track.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-import TrackModel from "../models/track.model";
+import TrackModel, { TrackJunctionModel } from "../models/track.model";
 
 type createTrackBody = {
   parent_id?: number;
-  spotify_uri: string;
   spotify_id: string;
   name: string;
 };
@@ -13,16 +12,39 @@ export const createTrack = async (
   res: Response<{ track: TrackModel } | { error: any }>
 ) => {
   try {
-    const track = await TrackModel.create({
-      spotify_id: req.body.spotify_id,
-      name: req.body.name,
-      parent_id: req.body.parent_id,
+    const oldTrack = await TrackModel.findOne({
+      where: { spotify_id: req.body.spotify_id },
     });
+    const newTrack =
+      !oldTrack &&
+      (await createTrackModel({
+        spotify_id: req.body.spotify_id,
+        name: req.body.name,
+        parent_id: req.body.parent_id,
+      }));
+    const track = oldTrack || newTrack;
+    if (!track) {
+      return res.status(500).json({ error: "no track created" });
+    }
+    if (req.body.parent_id) {
+      TrackJunctionModel.create({
+        parent_id: req.body.parent_id,
+        child_id: track.id,
+      });
+    }
+
+    track.reload();
     return res.status(201).json({ track });
   } catch (e) {
     return res.status(500).json({ error: e });
   }
 };
+async function createTrackModel({ spotify_id, name }: createTrackBody) {
+  return TrackModel.create({
+    spotify_id,
+    name,
+  });
+}
 
 export const getAllTracks = async (
   req: Request,
@@ -48,13 +70,13 @@ export const getTrackById = async (
 };
 export const getTracksBySpotifyId = async (
   req: Request<{ spotifyId: string }>,
-  res: Response<{ tracks: TrackModel[] } | { error: any }>
+  res: Response<{ track: TrackModel | null } | { error: any }>
 ) => {
   try {
-    const tracks = await TrackModel.findAll({
+    const track = await TrackModel.findOne({
       where: { spotify_id: req.params.spotifyId },
     });
-    return res.status(200).json({ tracks });
+    return res.status(200).json({ track });
   } catch (e) {
     console.error(e);
     return res.status(500).json({ error: e });
@@ -66,10 +88,19 @@ export const getTrackChildrenById = async (
   res: Response<{ tracks: TrackModel[] } | { error: any }>
 ) => {
   try {
-    const tracks = await TrackModel.findAll({
-      where: { parent_id: req.params.id },
-    });
-    return res.status(200).json({ tracks });
+    const track = await TrackModel.findByPk(req.params.id);
+    return res.status(200).json({ tracks: track?.children || [] });
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+};
+export const getTrackParentsById = async (
+  req: Request<{ id: number }>,
+  res: Response<{ tracks: TrackModel[] } | { error: any }>
+) => {
+  try {
+    const track = await TrackModel.findByPk(req.params.id);
+    return res.status(200).json({ tracks: track?.parents || [] });
   } catch (e) {
     return res.status(500).json({ error: e });
   }
--- api/app/src/models/track.model.ts
+++ api/app/src/models/track.model.ts
@@ -1,15 +1,31 @@
-import { Table, Column, Model, HasMany, PrimaryKey, ForeignKey, AutoIncrement, HasOne, BelongsTo } from "sequelize-typescript";
+import { Table, Column, Model, HasMany, PrimaryKey, ForeignKey, AutoIncrement, HasOne, BelongsToMany, Unique, AllowNull } from "sequelize-typescript";
+
+@Table
+export class TrackJunctionModel extends Model {
+
+  @ForeignKey(()=>TrackModel)
+  @AllowNull(false)
+  @Column
+  parent_id: number;
+
+  @ForeignKey(()=>TrackModel)
+  @AllowNull(false)
+  @Column
+  child_id: number;
+
+}
 
 @Table
 export default class TrackModel extends Model {
 
-  @ForeignKey(()=>TrackModel)
-  @Column
-  parent_id: number;
+  @BelongsToMany(() => TrackModel,()=> TrackJunctionModel,'parent_id')
+  parents: TrackModel[];
 
-  @BelongsTo(() => TrackModel)
-  track: TrackModel;
+  @BelongsToMany(() => TrackModel,()=> TrackJunctionModel,'child_id')
+  children: TrackModel[];
 
+
+  @Unique
   @Column
   spotify_id: string;
 
