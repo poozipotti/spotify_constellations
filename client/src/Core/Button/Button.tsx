085471821e44e56d0a5b7e0fb7f5a17a15b87e7a
George Beckert <georgebeckert@gmail.com>
2024-01-17 17:19:53 -0500

    [client] infinite loader for search

--- client/src/Core/Button/Button.tsx
+++ client/src/Core/Button/Button.tsx
@@ -1,33 +1,32 @@
-import React, { PropsWithChildren, PropsWithRef, forwardRef } from "react";
+import React, { PropsWithChildren, forwardRef } from "react";
 
 type props = {
   onClick?: () => void;
   hideBorder?: boolean;
   isLoading?: boolean;
 };
-export const Button = forwardRef<
-  HTMLButtonElement,
-  PropsWithChildren<props>
->(({ children, onClick, hideBorder, isLoading }, ref) => {
-  return (
-    <button
-      ref={ref}
-      onClick={onClick}
-      disabled={isLoading}
-      className={`
+export const Button = forwardRef<HTMLButtonElement, PropsWithChildren<props>>(
+  ({ children, onClick, hideBorder, isLoading }, ref) => {
+    return (
+      <button
+        ref={ref}
+        onClick={onClick}
+        disabled={isLoading}
+        className={`
         ${hideBorder ? "" : "border border-primary-light p-2 px-6"}
         text-primary-light
         uppercase 
         
       `}
-    >
-      {isLoading ? (
-        <div className={`flex justify-center w-full`}>
-          <div className="h-6 w-6 border-b-2 border-t-2 rounded-full animate-spin border-primary-light" />
-        </div>
-      ) : (
-        children
-      )}
-    </button>
-  );
-});
+      >
+        {isLoading ? (
+          <div className={`flex justify-center w-full`}>
+            <div className="h-6 w-6 border-b-2 border-t-2 rounded-full animate-spin border-primary-light" />
+          </div>
+        ) : (
+          children
+        )}
+      </button>
+    );
+  }
+);
